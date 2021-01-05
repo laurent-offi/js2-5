769cb4f5543f4ebfb7e531fd4352c97185661e81
laurent-offi <laurent.courcelle.lh@gmail.com>
2021-01-05 10:29:01 +0100

    Finission de l'exercice 5 js2

---
+++ script/result.js
@@ -0,0 +1,7 @@
+image1.onmouseover = function() {  image1.src = "images/image1_2.jpg"; }
+image2.onmouseover = function() {  image2.src = "images/image2_2.jpg"; }
+image3.onmouseover = function() {  image3.src = "images/image3_2.jpg"; }
+image4.onmouseover = function() {  image4.src = "images/image4_2.jpg"; }
+image5.onmouseover = function() {  image5.src = "images/image5_2.jpg"; }
+
+
